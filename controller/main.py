7439e3d961007631be401128953c16450240af76
saturn691 <williamhuynh2002@gmail.com>
2023-05-22 12:48:27 +0100

    Added a working controller module

--- controller/main.py
+++ controller/main.py
@@ -1,17 +1,32 @@
 import socket
 from udp_connect import fetchUDPAddress
-import controls
+from xbox_controller import XboxController
+import time
 
 # Define the target device and port
 target_device = "Adafruit Metro M0"
 TARGET_PORT = 6969
 TARGET_IP = "192.169.0.8"
 
+controller = XboxController()
+
 while True:
-    output = controls.get_button_events()
-    if output != None:
-        print(output)
-        
+    axis_inputs = controller.get_axis_inputs()
+    button_inputs = controller.get_button_inputs()
+
+    # Print the axis inputs
+    print("Axis Inputs:")
+    for axis_name, axis_value in axis_inputs.items():
+        print(f"{axis_name}: {axis_value}")
+
+    # Print the button inputs
+    pressed_buttons = [button_name for button_name, button_state in button_inputs.items() if button_state]
+    if pressed_buttons:
+        print("Buttons pressed:", ", ".join(pressed_buttons))
+
+    # Rest of your code here...
+    time.sleep(1)
+   
 
 # # Create a UDP socket
 # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
